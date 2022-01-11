f4941b66b97581521b8db829783b8e3fb237cb82
benyamin-nayudi <benyamin.naudi@gmail.com>
2022-01-11 20:08:38 +0330

    different ways of exports

--- 3-modules.js
+++ 3-modules.js
@@ -3,7 +3,9 @@
 
 const names = require('./4-names')
 const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavor')
 
+console.log(data)
 sayHi('beny')
 sayHi(names.john)
 sayHi(names.peter)
---
+++ 6-alternative-flavor.js
@@ -0,0 +1,8 @@
+// we can use this syntax too.
+
+module.exports.items = ['item1' , 'item2']
+const person = {
+    name : 'dan'
+}
+
+module.exports.singlePerson = person
