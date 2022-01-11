16a0e5ac44ab11427f0790152a583c82cae548a2
benyamin-nayudi <benyamin.naudi@gmail.com>
2022-01-11 20:15:11 +0330

    require a result

--- 3-modules.js
+++ 3-modules.js
@@ -5,7 +5,13 @@ const names = require('./4-names')
 const sayHi = require('./5-utils')
 const data = require('./6-alternative-flavor')
 
-console.log(data)
-sayHi('beny')
-sayHi(names.john)
-sayHi(names.peter)
+// console.log(data)
+// sayHi('beny')
+// sayHi(names.john)
+// sayHi(names.peter)
+
+
+
+
+// we can also import the result of a specific function => when you import a module , you actually invoke it. when node export a module , it actually wraps it in a function thats why when we require it we invoke it
+require('./7-mind-grenade')
---
+++ 7-mind-grenade.js
@@ -0,0 +1,9 @@
+const num1 = 5 ;
+const num2 = 10;
+
+function addValues (){
+    console.log( num1 + num2)
+}
+
+// we can import the value to another file => 3-modules.js
+addValues()
