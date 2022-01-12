671115f957354747c9f45ee8b2253a2adc3ade59
benyamin-nayudi <benyamin.naudi@gmail.com>
2022-01-12 21:58:06 +0330

    theroy

---
+++ 18-theroy-internet.js
@@ -0,0 +1,14 @@
+/*
+both the request and response has similar properties.
+
+ - user send the request
+ - we should set a proper server to return the correct response
+ - the request has some methods => get , post , delete , put
+ - headers are optional and they give us a more info about the request
+ - when the user wants to add some info to the server , the data will be in the body (payload)
+
+ - the response message is what we gonna be reading.
+ - status code: 200 (req was successfully) and ... . 
+ - headers : info about our response message (Content-Type : text/html(for webpage) or application/json (for data))
+ - 
+*/ 
