72eefb871dafb6313a14d6f30485288eee3b3d30
benyamin-nayudi <benyamin.naudi@gmail.com>
2022-01-12 08:56:36 +0330

    os module

---
+++ 8-os-module.js
@@ -0,0 +1,21 @@
+//  os module 
+const os = require('os')
+// console.log(os)
+
+// info about current user
+const user = os.userInfo()
+console.log(user)
+
+
+// method returns the system uptime in seconds
+console.log('system uptime' + os.uptime()/60/60)
+
+
+const currentOs  = {
+    name : os.type(),
+    release: os.release(),
+    totalMem : os.totalmem(),
+    freeMem : os.freemem()
+}
+console.log(currentOs)
+
---
+++ app.js
@@ -0,0 +1,5 @@
+const path = require('path')
+
+// return the file system path separator
+console.log(path.sep)
+console.log()
