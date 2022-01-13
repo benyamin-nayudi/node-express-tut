7c641ee339b62232b83f4b42081ee298800beb71
benyamin-nayudi <benyamin.naudi@gmail.com>
2022-01-13 11:12:04 +0330

    app.all

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -5,9 +5,28 @@ const app = express();
 
 
 //* we have some methods that we use the most:
-// app.get
-// app.put
+// app.get(path , callback (invoked every time the user perform a get request))
+app.get('/' , (req , res) =>{
+    res.send('home page')
+})
+
+app.get('/about' , (req , res) =>{
+    res.send('about page')
+})
+
+
+
+
+//* this handles all http request (get , post ...)
+// app.all(path (* : all) , callback ) => if i can not reach the resource so i send back this.
+// we can add our status here and chain it.
+app.all("*" , (req , res) =>{
+    res.status(404).send('<h1>page not found</h1>')
+})
+
 // app.post
+// app.put
+// app.all
 // app.delete
 // app.use
 
