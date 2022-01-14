4d4bd01a037e1fb123a7094d635c5be0c18692e4
benyamin-nayudi <benyamin.naudi@gmail.com>
2022-01-14 08:14:14 +0330

    delete

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -79,7 +79,17 @@ app.put('/api/people/:id' , (req , res) =>{
 
 
 // delete
-app.delete('/api/people/:id')
+app.delete('/api/people/:id' , (req , res) =>{
+    const person = people.find( person => person.id === Number(req.params.id))
+
+    if(!person) {
+        return res.status(400).json({success : false , msg : `no person with id : ${req.params.id}`})
+    }
+
+    const newPeople = people.filter(person => person.id !== Number(req.params.id))
+
+    return res.status(200).json({success: true , data : newPeople})
+})
 
 
 
