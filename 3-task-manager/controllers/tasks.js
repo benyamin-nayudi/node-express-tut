96849b59d57066345930a5b1a66a6bbe5c1cc254
benyamin-nayudi <benyamin.naudi@gmail.com>
2022-01-15 08:31:19 +0330

    route setup

---
+++ 3-task-manager/controllers/tasks.js
@@ -0,0 +1,28 @@
+const getAllTasks = (req , res) =>{
+    res.send('get all tasks')
+}
+
+const getTask = (req , res ) =>{
+    res.json({ id : req.params.id })
+}
+const createTask = (req , res ) =>{
+    res.json(req.body)
+}
+
+const updateTask  = (req , res) =>{
+    res.send('update  Task')
+}
+
+const deleteTask = (req , res) =>{
+    res.send('delete Task')
+}
+
+
+
+module.exports = {
+    getAllTasks,
+    getTask,
+    createTask,
+    updateTask,
+    deleteTask
+}
