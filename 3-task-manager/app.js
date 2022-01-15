b3cded5aa9f82fad1b14723b67bee8f747cda217
benyamin-nayudi <benyamin.naudi@gmail.com>
2022-01-15 08:04:54 +0330

    setup a basis api

--- 3-task-manager/app.js
+++ 3-task-manager/app.js
@@ -1,7 +1,11 @@
 const express = require('express')
-
+const tasks = require('./routes/tasks')
 
 const app = express();
+
+// middleware : if we don't use this we won't have our data in the req.body
+app.use(express.json())
+
 
 
 app.get('/hello' , (req , res) =>{
@@ -9,7 +13,25 @@ app.get('/hello' , (req , res) =>{
 })
 
 
+
+app.use('/api/v1/tasks' , tasks)
+
+
+
+
+
+
+
+
+
+
+
+
 const port = 3000;
+
+
+
+
 
 app.listen(port , ()=>{
     console.log('server running on port 3000...')
